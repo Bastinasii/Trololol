707a9aeea3ede3fd7da577449c5c9bf6a60f94e0
gaby15a <alex.rdcn@gmail.com>
2014-05-29 11:24:51 +0300

    bla

---
+++ Antivirus/src/antivirus/Antivirus.java
@@ -0,0 +1,37 @@
+/*=======
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package antivirus;
+
+/**
+ *
+ * @author Alex
+ */
+import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+//import java.nio.file.Files;
+
+public class Antivirus 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+//        JFrame message = new JFrame();
+//        GenCrc crc = new GenCrc();
+//        Scanner read = new Scanner(System.in);
+//        
+//        String fileToCrc = "D:\\Downloads\\Arrow.S02E21.720p.HDTV.X264-DIMENSION\\Arrow.S02E21.720p.HDTV.X264-DIMENSION.mkv";
+
+//        JOptionPane.showMessageDialog(message, crc.crcInput(fileToCrc));
+        
+    }
+}
