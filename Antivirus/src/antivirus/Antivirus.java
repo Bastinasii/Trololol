287d0354d24fae207c843a4cc10e9cea38921c11
gaby15a <alex.rdcn@gmail.com>
2014-05-29 12:40:42 +0300

    lalala

--- Antivirus/src/antivirus/Antivirus.java
+++ Antivirus/src/antivirus/Antivirus.java
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-
-//package antivirus;
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,18 +5,13 @@
  */
 
 package antivirus;
->>>>>>> 1c95945d6df0752fa0237250613fd78cf191a715
 
 /**
  *
  * @author Alex
  */
-<<<<<<< HEAD
-/*import java.io.IOException;
-=======
 
 import java.io.IOException;
->>>>>>> 1c95945d6df0752fa0237250613fd78cf191a715
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
@@ -32,13 +23,8 @@ public class Antivirus
     /**
      * @param args the command line arguments
      */
-<<<<<<< HEAD
-   //* public static void main(String[] args) 
-    //{
-=======
     public static void main(String[] args) 
     {
->>>>>>> 1c95945d6df0752fa0237250613fd78cf191a715
 //        JFrame message = new JFrame();
 //        GenCrc crc = new GenCrc();
 //        Scanner read = new Scanner(System.in);
@@ -46,12 +32,5 @@ public class Antivirus
 //        String fileToCrc = "D:\\Downloads\\Arrow.S02E21.720p.HDTV.X264-DIMENSION\\Arrow.S02E21.720p.HDTV.X264-DIMENSION.mkv";
 
 //        JOptionPane.showMessageDialog(message, crc.crcInput(fileToCrc));
-        
-<<<<<<< HEAD
-  //  }
-//}
-//>>>>>>> abd14a397ddb83c1b79e8a271a9bac29cf2e2443:Antivirus/src/antivirus/Antivirus.java
-=======
     }
 }
->>>>>>> 1c95945d6df0752fa0237250613fd78cf191a715
